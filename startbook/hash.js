127c4cb6070f844bdf8a31d12290b18d7e1afb9c
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-06-06 03:55:10 +0000

    ClickUp #qnvuj 入力結果を画面内に表示する

--- startbook/hash.js
+++ startbook/hash.js
@@ -5,4 +5,8 @@
 const ShowHash = () => {
 	const form = document.forms.hashForm;
 	console.log(form.inputHash.value);
+	const div = document.createElement('div')
+	div.textContent = form.inputHash.value;
+
+	element.parentNode.insertBefore(div, element.nextSibling);
 }
